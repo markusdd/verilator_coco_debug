201119eeeea6d7242aaad276ec07d03ac7000380
Markus Krause <krause.markus@gmx.com>
2021-02-07 15:41:26 +0100

    enhance

--- debug_verilator_coco.py
+++ debug_verilator_coco.py
@@ -1,3 +1,5 @@
+# NOT FOR REDISTRIBUTION AND PRACTICAL USE IN ANY DESIGNS, ONLY FOR COCOTB/VERILTOR INTERACTION DEGUGGING
+
 import random
 import cocotb
 import subprocess
@@ -13,8 +15,8 @@ import asyncio
 
 async def write_fifo(dut, vals):
     w_cnt = 0
-    cocotb.log.info(f"In write proc.")
     while True:
+        cocotb.log.info(f"In write proc.")
         for _ in range(random.randrange(1, 32)):
             await FallingEdge(dut.clk_w_i)
         if dut.full_o == 0:
@@ -28,8 +30,8 @@ async def write_fifo(dut, vals):
 
 async def read_fifo(dut, vals):
     r_cnt = 0
-    cocotb.log.info(f"In read proc.")
     while True:
+        cocotb.log.info(f"In read proc.")
         for _ in range(random.randrange(0, 12)):
             await FallingEdge(dut.clk_r_i)
         if dut.empty_o == 0:
@@ -38,76 +40,28 @@ async def read_fifo(dut, vals):
             await RisingEdge(dut.clk_r_i)
             dut.ren_i <= 0
             await FallingEdge(dut.clk_r_i)
-            cocotb.log.info(f"Read Data: {dut.rdata_o}")
+            assert dut.rdata_o == vals[r_cnt], f"Read Data mismatch at value {r_cnt}, got {hex(int(dut.rdata_o))} but expected {hex(int(vals[r_cnt]))}"
             r_cnt = r_cnt + 1
 
 
 @cocotb.test()
 async def test_afifo(dut):
-    """ Test that data is being passed across clock domains. """
 
-    clk_w = Clock(dut.clk_w_i, 10, units="ns")  # Create a 10ns period clock on port clk
-    clk_w_fork = cocotb.fork(clk_w.start())  # Start the write clock
-    clk_r = Clock(dut.clk_r_i, 37, units="ns")  # Create a 37ns period clock on port clk
-    clk_r_fork = cocotb.fork(clk_r.start())  # Start the read clock
-    vals = [random.randrange(1, 2**32) for _ in range(2**16)]
+    vals = [random.randrange(1, 2**41) for _ in range(2**16)]
 
-    # reset, disable divs
+    # reset
     cocotb.log.info(f"Reset AFIFO on both sides.")
     dut.rst_w_an_i <= 0
     dut.rst_r_an_i <= 0
-    await FallingEdge(dut.clk_w_i)
-
-    # deassert reset, check non full, but empty
+    await Timer(1, units='us')
     dut.rst_w_an_i <= 1
     dut.rst_r_an_i <= 1
-
-    # fill
-    cocotb.log.info(f"Fill FIFO with random values.")
-    for i in range(4):
-        await FallingEdge(dut.clk_w_i)
-        assert dut.full_o == 0, f"Status full_o was asserted already after {i} samples."
-        dut.wen_i <= 1
-        dut.wdata_i <= vals[i]
-        await RisingEdge(dut.clk_w_i)
-        dut.wen_i <= 0
-        if i == 0:
-            # check after first write that the empty flag goes low on read side (wait for sync)
-            for i in range(3):
-                await FallingEdge(dut.clk_r_i)
-            assert dut.empty_o == 0, f"Status empty_o was still asserted after {i} writes."
-            await FallingEdge(dut.clk_w_i)
-    await FallingEdge(dut.clk_w_i)
-
-    # read until empty
-    cocotb.log.info(f"Read back previously written data.")
-    for i in range(4):
-        await FallingEdge(dut.clk_r_i)
-        assert dut.empty_o == 0, f"Status empty_o was asserted already after {i} samples."
-        dut.ren_i <= 1
-        await RisingEdge(dut.clk_r_i)
-        dut.ren_i <= 0
-        await FallingEdge(dut.clk_r_i)
-        assert dut.rdata_o == vals[i], f"Read Data mismatch at read {i}, got {hex(int(dut.rdata_o))} but expected {hex(int(vals[i]))}"
-        if i == 0:
-            # check after first read that the full flag goes low on write side (wait for sync)
-            for i in range(3):
-                await FallingEdge(dut.clk_w_i)
-            assert dut.full_o == 0, f"Status full_o was still asserted after {i} reads."
-            await FallingEdge(dut.clk_r_i)
-    await FallingEdge(dut.clk_r_i)
-
-    #shut down clocks in preparation for random testing
-    await FallingEdge(dut.clk_r_i)
-    clk_r_fork.kill()
-    await FallingEdge(dut.clk_w_i)
-    clk_w_fork.kill()
     await Timer(1, units='us')
 
     # randomize
     #for w_ns, r_ns in [(3, 7), (20, 20), (50, 11)]:
-    #for w_ns, r_ns in [(20, 20), (3, 7), (50, 11)]:
-    for w_ns, r_ns in [(50, 11), (3, 7), (20, 20)]:
+    for w_ns, r_ns in [(20, 20), (3, 7), (50, 11)]:
+    #for w_ns, r_ns in [(50, 11), (3, 7), (20, 20)]:
         cocotb.log.info(f"Test random read/write. Write Period is {w_ns}ns, read period is {r_ns}ns.")
         clk_w = Clock(dut.clk_w_i, w_ns, units="ns")
         clk_w_fork = cocotb.fork(clk_w.start())  # Start the write clock
@@ -118,16 +72,9 @@ async def test_afifo(dut):
         await ClockCycles(dut.clk_r_i, 1000)
         await FallingEdge(dut.clk_w_i)
         clk_w_fork.kill() # enforce reading until empty
-        await RisingEdge(dut.empty_o)
+        if dut.empty_o != 1:
+            await RisingEdge(dut.empty_o)
         clk_r_fork.kill()
         w_fork.kill()
         r_fork.kill()
         await Timer(1, units='us')
-
-
-
-
-
-
-
-
--- debug_verilator_coco.v
+++ debug_verilator_coco.v
@@ -1,21 +1,27 @@
-module debug_verilator_coco (
-  input clk_w_i,
-  input rst_w_an_i,
-  input wen_i,
-  input [31:0] wdata_i,
-  input clk_r_i,
-  input rst_r_an_i,
-  input ren_i,
-  output full_o,
-  output [31:0] rdata_o,
-  output empty_o
+//NOT FOR REDISTRIBUTION AND PRACTICAL USE IN ANY DESIGNS, ONLY FOR COCOTB/VERILTOR INTERACTION DEGUGGING
+
+module debug_verilator_coco #(
+  parameter DATA_WIDTH = 42,
+  parameter DEPTH = 16,
+  parameter RDATA_REG = 1
+) (
+  //write side
+  input logic clk_w_i,
+  input logic rst_w_an_i,
+  input logic wen_i,
+  input logic [DATA_WIDTH-1:0] wdata_i,
+  output logic full_o,
+  output logic werr_o,
+  //read side
+  input logic clk_r_i,
+  input logic rst_r_an_i,
+  input logic ren_i,
+  output logic [DATA_WIDTH-1:0] rdata_o,
+  output logic empty_o,
+  output logic rerr_o
 );
 
-  //THIS IS A DEBUG DESIGN, NOT A FUNCTIONAL FIFO!!!
-
-  localparam DATA_WIDTH = 32;
-  localparam DEPTH = 16;
-  localparam RDATA_REG = 1;
+  //declarations
   localparam ADDR_WIDTH_P = $clog2(DEPTH);
 
   logic [DATA_WIDTH-1:0] fifo_mem_r [DEPTH-1:0];
@@ -26,48 +32,115 @@ module debug_verilator_coco (
   logic empty_s;
   logic empty_r;
 
-  logic [ADDR_WIDTH_P:0] wpointer_next_s;
-  logic [ADDR_WIDTH_P:0] wpointer_r;
+  logic [ADDR_WIDTH_P:0] bin_wpointer_next_s;
+  logic [ADDR_WIDTH_P:0] gray_wpointer_next_s;
+  logic [ADDR_WIDTH_P:0] bin_wpointer_r;
+  logic [ADDR_WIDTH_P:0] gray_wpointer_r;
+  logic [ADDR_WIDTH_P:0] gray_wpointer_sync_s;
 
-  logic [ADDR_WIDTH_P:0] rpointer_next_s;
-  logic [ADDR_WIDTH_P:0] rpointer_r;
+  logic [ADDR_WIDTH_P:0] bin_rpointer_next_s;
+  logic [ADDR_WIDTH_P:0] gray_rpointer_next_s;
+  logic [ADDR_WIDTH_P:0] bin_rpointer_r;
+  logic [ADDR_WIDTH_P:0] gray_rpointer_r;
+  logic [ADDR_WIDTH_P:0] gray_rpointer_sync_s;
 
-  assign empty_o = 1'b0;
-  assign full_o = 1'b0;
-
-  always @(posedge clk_w_i) begin : fifo_mem_w_proc
-    if (wen_i == 1'b1) begin
-      fifo_mem_r[wpointer_r[ADDR_WIDTH_P-1:0]] <= wdata_i;
+  always_ff @(posedge clk_w_i) begin : fifo_mem_w_proc
+    if ((wen_i == 1'b1) && (full_r == 1'b0)) begin
+      fifo_mem_r[bin_wpointer_r[ADDR_WIDTH_P-1:0]] <= wdata_i;
     end
   end
 
-  always @(posedge clk_r_i) begin : fifo_mem_r_proc
-    if (ren_i == 1'b1) begin
-      rdata_r <= fifo_mem_r[rpointer_r[ADDR_WIDTH_P-1:0]];
+  if (RDATA_REG == 1) begin
+    always_ff @(posedge clk_r_i) begin : fifo_mem_r_proc
+      if (ren_i == 1'b1) begin
+         rdata_r <= fifo_mem_r[bin_rpointer_r[ADDR_WIDTH_P-1:0]];
+      end
     end
+
+    assign rdata_o = rdata_r;
+  end else if (RDATA_REG == 0) begin
+    assign rdata_o = fifo_mem_r[bin_rpointer_r[ADDR_WIDTH_P-1:0]];
+  end else begin
+    $error("SVB IP ERROR: RDATA_REG parameter has to be either 0 or 1.");
   end
 
-  assign rdata_o = rdata_r;
-
-  always @(*) begin : pointer_next_proc
-    rpointer_next_s = rpointer_r + {{ADDR_WIDTH_P{1'b0}}, 1'b1 & ren_i};
-    wpointer_next_s = wpointer_r + {{ADDR_WIDTH_P{1'b0}}, 1'b1 & wen_i};
+  always_comb begin : pointer_next_proc
+      bin_rpointer_next_s = bin_rpointer_r + {{ADDR_WIDTH_P{1'b0}}, 1'b1 & ren_i & ~empty_r};
+      gray_rpointer_next_s = (bin_rpointer_next_s>>1) ^ bin_rpointer_next_s;
+      bin_wpointer_next_s = bin_wpointer_r + {{ADDR_WIDTH_P{1'b0}}, 1'b1 & wen_i & ~full_r};
+      gray_wpointer_next_s = (bin_wpointer_next_s>>1) ^ bin_wpointer_next_s;
+      empty_s = (gray_rpointer_next_s == gray_wpointer_sync_s);
+      full_s = (gray_wpointer_next_s == {~gray_rpointer_sync_s[ADDR_WIDTH_P:ADDR_WIDTH_P-1],
+                                          gray_rpointer_sync_s[ADDR_WIDTH_P-2:0]});
   end
 
-  always @(posedge clk_w_i or negedge rst_w_an_i) begin : w_pointer_proc
+  always_ff @(posedge clk_w_i or negedge rst_w_an_i) begin : w_pointer_proc
     if (rst_w_an_i == 1'b0) begin
-      wpointer_r <= {ADDR_WIDTH_P+1{1'b0}};
-    end else if (wen_i == 1'b1) begin
-      wpointer_r <= wpointer_next_s;
+      bin_wpointer_r <= {ADDR_WIDTH_P+1{1'b0}};
+      gray_wpointer_r <= {ADDR_WIDTH_P+1{1'b0}};
+    end else if ((wen_i == 1'b1) && (full_r == 1'b0)) begin
+      bin_wpointer_r <= bin_wpointer_next_s;
+      gray_wpointer_r <= gray_wpointer_next_s;
     end
   end
 
-  always @(posedge clk_r_i or negedge rst_r_an_i) begin : r_pointer_proc
+  always_ff @(posedge clk_r_i or negedge rst_r_an_i) begin : r_pointer_proc
     if (rst_r_an_i == 1'b0) begin
-      rpointer_r <= {ADDR_WIDTH_P+1{1'b0}};
-    end else if (ren_i == 1'b1) begin
-      rpointer_r <= rpointer_next_s;
+      bin_rpointer_r <= {ADDR_WIDTH_P+1{1'b0}};
+      gray_rpointer_r <= {ADDR_WIDTH_P+1{1'b0}};
+    end else if ((ren_i == 1'b1) && (empty_r == 1'b0)) begin
+      bin_rpointer_r <= bin_rpointer_next_s;
+      gray_rpointer_r <= gray_rpointer_next_s;
     end
   end
+
+  always_ff @(posedge clk_r_i or negedge rst_r_an_i) begin : proc_empty
+    if(rst_r_an_i == 1'b0) begin
+      empty_r <= 1'b1;
+      rerr_o <= 1'b0;
+    end else begin
+      empty_r <= empty_s;
+      rerr_o <= ren_i & empty_r;
+    end
+  end
+
+  assign empty_o = empty_r;
+
+  always_ff @(posedge clk_w_i or negedge rst_w_an_i) begin : proc_full
+    if(rst_w_an_i == 1'b0) begin
+      full_r <= 1'b0;
+      werr_o <= 1'b0;
+    end else begin
+      full_r <= full_s;
+      werr_o <= wen_i & full_r;
+    end
+  end
+
+  assign full_o = full_r;
+
+  genvar i;
+  generate
+    for (i = 0; i<=ADDR_WIDTH_P; i++) begin : pointer_sync_gen
+
+      sync #(
+        .RST_VAL (1'b0)
+      ) u_svb_sync_gray_wpointer (
+        .clk_i    (clk_r_i                ),
+        .rst_an_i (rst_r_an_i             ),
+        .d_i      (gray_wpointer_r[i]     ),
+        .d_o      (gray_wpointer_sync_s[i])
+      );
+
+      sync #(
+        .RST_VAL (1'b0)
+      ) u_svb_sync_gray_rpointer (
+        .clk_i    (clk_w_i                ),
+        .rst_an_i (rst_w_an_i             ),
+        .d_i      (gray_rpointer_r[i]     ),
+        .d_o      (gray_rpointer_sync_s[i])
+      );
+
+    end
+  endgenerate
 
 endmodule
